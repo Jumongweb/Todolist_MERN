db778a0fb01bdccdd2185e5de1b738eff74110a3
Lawal Toheeb <lawaltoheeb36@gmail.com>
2025-07-02 22:40:57 +0100

    config

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
